6af46e29f936f7947dea9e1beef56dceaa53db98
limapereiralucas <limapereiralucas@gmail.com>
2025-09-16 14:42:29 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -9,9 +9,25 @@ pkg load optim
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+#função otimização:
+f=[10,14]';
 
-% digite seu codigo aqui
+#chutes iniciais:
+x0=[1,1]';
 
+#restrições de desigualdade Ax<=b
+A=[-0.3,-0.2;
+  -0.2,-0.25;
+  -0.25,-0.3;
+  1,0;
+  0,1];
+b=[-15;-10;-12;20;50];
+
+#nao tem restrições de igualdade
+
+x_min_valores=zeros(2,1);
+x_max_valores=1000*ones(2,1);
+x=linprog(f,A,b)
 
 % mantenha essas duas linhas finais
 blendA = x(1);
